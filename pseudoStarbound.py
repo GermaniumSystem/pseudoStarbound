f7aea4798b63169c54977e2232dbf877a22a688b
GermaniumSystem <GermaniumSystem@users.noreply.github.com>
2017-05-07 03:41:13 -0400

    Attempted some cleanup.

--- pseudoStarbound.py
+++ pseudoStarbound.py
@@ -1,14 +1,13 @@
 #!/usr/bin/python3
 
 """
-pseudoStarbound - An extremely basic server intended to act as a failover for large Starbound servers.
+pseudoStarbound - A simple Starbound-compatible server designed to function as a quick-n'-dirty fallback.
 """
 
 import asyncio
 import configparser
 import datetime
 import os
-import time
 
 
 config_file = "config/config.cfg"
@@ -17,35 +16,21 @@ example_cfg = "config/example.cfg"
 packet_ids = {"proto_request": b'\x00', "proto_response": b'\x01', "client_connect": b'\x0b', "connect_failure": b'\x04'}
 payloads = {"good_proto": b'\x02\x01', "bad_proto": b'\x02\x00'} # VLQ (\x02) + True/false.
 
-config = configparser.SafeConfigParser()
-try:
-    config.read(config_file)
-    pid_file = config["main"]["pid_file"]
-    log_file = config["main"]["log_file"]
-    status_file = config["main"]["status_file"]
-    proto_version = int(config["main"]["proto_version"])
-    timeout = int(config["main"]["timeout"])
-    bind_port = int(config["main"]["bind_port"])
-    bind_ip = config["main"]["bind_ip"]
-    for option in config["main"]:
-        if not config["main"][option]:
-            raise ValueError("Config options cannot be null!")
-except Exception as e:
-    print("Failed to read {}! Please reference {} for correct syntax.".format(config_file, example_cfg))
-    print(e)
-    exit(1)
 
 def log(msg):
+    """Given a msg, print it to stdout and write it to log_file."""
     print("{}: {}".format(datetime.datetime.utcnow().isoformat(), msg))
-    log_handler = open(log_file, 'a')
-    log_handler.write("{}: {}\n".format(datetime.datetime.utcnow().isoformat(), msg))
-    log_handler.close()
+    lf = open(log_file, 'a')
+    lf.write("{}: {}\n".format(datetime.datetime.utcnow().isoformat(), msg))
+    lf.close()
+
 
 # Shamefully taken from https://github.com/StarryPy/StarryPy3k/ because VLQs suck. :c
-def buildSignedVLQ(obj):
+def build_signed_VLQ(length):
+    """Given a length, return a signed VLQ representation of the value."""
     result = bytearray()
-    value = abs(int(obj) * 2)
-    if obj == 0:
+    value = abs(int(length) * 2)
+    if length == 0:
         result = bytearray(b'\x00')
     else:
         while value > 0:
@@ -59,8 +44,10 @@ def buildSignedVLQ(obj):
             result[-1] ^= 0x80
     return bytes(result)
 
+
 # Again, shamefully taken from StarryPy3k, because VLQs suck.
-def readSignedVLQ(reader):
+def read_signed_VLQ(reader):
+    """Given a reader, read data as a signed VLQ and return its value."""
     d = b""
     v = 0
     while True:
@@ -76,13 +63,40 @@ def readSignedVLQ(reader):
     else:
         return -((v >> 1) + 1)
 
+
 def read_packet(reader):
+    """
+    Given a reader, read a packet_id, decode the VLQ, and read contents.
+
+    :param reader: Stream to receive packets from.
+    :return: Tuple: The packet_id and data from the packet.
+    """
     packet_id = (yield from reader.readexactly(1))
-    packet_len = (yield from readSignedVLQ(reader))
+    packet_len = (yield from read_signed_VLQ(reader))
     data = (yield from reader.readexactly(abs(packet_len)))
     return packet_id, data
 
+
 async def handle_connection(reader, writer):
+    """
+    Given a reader and writer, wait for new connections and respond with a
+    protocol-compliant disconnect upon successful connection.
+    Handle connections in two steps:
+    1. Respond to a protocol_request:
+        - If the protocol version is expected, send a protocol_response
+          containing the "good_proto" payload.
+        - If the protocol version is unexpected, send a protocol_response
+          containing the "bad_proto" payload.
+    2. Respond to a client_connect:
+        - Load a status_message from the status_file and send a
+          connect_failure containing the encoded status_message.
+    If an unexpected packet_id is received at any point, disconnect.
+
+    :param reader: Stream to receive packets from.
+    :param writer: Stream to send packets to.
+    :return: Null.
+    """
+
     host = writer.get_extra_info('peername')[0]
     log("Connection received from {}.".format(host))
     try:
@@ -95,11 +109,11 @@ async def handle_connection(reader, writer):
         try:
             proto = int.from_bytes(data[2:], byteorder='big')
         except:
-            log("- Failed to parse protocolRequest protocol ({}). Aborting connection to {}...".format(data[2:],host))
+            log("- Failed to parse protocol_request protocol ({}). Aborting connection to {}...".format(data[2:],host))
             writer.close()
             return
         if proto == proto_version:
-            log("- Recieved expected protocol from {}. Continuing...".format(host))
+            log("- Received expected protocol from {}. Continuing...".format(host))
             writer.write(packet_ids["proto_response"] + payloads["good_proto"])
             await writer.drain()
         else:
@@ -109,7 +123,7 @@ async def handle_connection(reader, writer):
             writer.close()
             return
     else:
-        log("- Unepected packet ID {}. Aborting connection to {}...".format(packet_id,host))
+        log("- Unexpected packet ID {}. Aborting connection to {}...".format(packet_id,host))
         writer.close()
         return
 
@@ -122,14 +136,13 @@ async def handle_connection(reader, writer):
     if packet_id == packet_ids["client_connect"]:
         log("- Disconnecting {} with status message and aborting connection.".format(host))
         try:
-            # Why open, read, and close the file every connection? It's often re-written by other utilities and this tends to break stuff.
-            # This may seem inefficient, but I've tested it with ~1000 protocol-compliant client connections per second and experienced no issues.
-            f = open(status_file, 'r')
-            status_msg = bytes(f.read(), 'utf-8')
+            sf = open(status_file, 'r')
+            status_msg = bytes(sf.read(), 'utf-8')
+            sf.close()
         except:
             status_msg = bytes("^white; The server is currently down\n\n^red;(And the pseudoServer is misconfigured)", 'utf-8')
             log("! Unable to read status message from {}!".format(status_file))
-        vlq = buildSignedVLQ(len(status_msg) + 1)
+        vlq = build_signed_VLQ(len(status_msg) + 1)
         length = len(status_msg).to_bytes(1, byteorder='big')
         writer.write(packet_ids["connect_failure"] + vlq + length + status_msg)
         await writer.drain()
@@ -140,11 +153,21 @@ async def handle_connection(reader, writer):
         writer.close()
         return
 
+
 def main():
+    """
+    Start a TCP server and wait for Starbound clients to connect. Upon a
+    successful connection, send a compliant response to the client and
+    disconnect it with a status message.
+    Before starting the server:
+        Write the PID to pid_file.
+    After receiving a SIGTERM:
+        Truncate pid_file.
+    """
     pid = os.getpid()
-    pid_handler = open(pid_file, 'w')
-    pid_handler.write(str(pid))
-    pid_handler.close()
+    pf = open(pid_file, 'w')
+    pf.write(str(pid))
+    pf.close()
 
     loop = asyncio.get_event_loop()
     coro = asyncio.start_server(handle_connection, bind_ip, bind_port, loop=loop)
@@ -162,9 +185,26 @@ def main():
     loop.run_until_complete(server.wait_closed())
     loop.close()
 
-    pid_handler = open(pid_file, 'w')
-    pid_handler.truncate()
-    pid_handler.close
+    pf = open(pid_file, 'w')
+    pf.truncate()
+    pf.close
 
 if __name__ == '__main__':
+    config = configparser.SafeConfigParser()
+    try:
+        config.read(config_file)
+        pid_file      =     config["main"]["pid_file"]
+        log_file      =     config["main"]["log_file"]
+        status_file   =     config["main"]["status_file"]
+        proto_version = int(config["main"]["proto_version"])
+        timeout       = int(config["main"]["timeout"])
+        bind_port     = int(config["main"]["bind_port"])
+        bind_ip       =     config["main"]["bind_ip"]
+        for option in config["main"]:
+            if not config["main"][option]:
+                raise ValueError("Config options cannot be null!")
+    except Exception as e:
+        print("Failed to read {}! Please reference {} for correct syntax.".format(config_file, example_cfg))
+        print(e)
+        exit(1)
     main()
